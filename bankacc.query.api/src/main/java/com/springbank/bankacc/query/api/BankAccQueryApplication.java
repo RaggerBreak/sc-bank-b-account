ddc2c1281fc00072a62c55fd88d77b8c9c821278
ragger.break <ragger.break@gmail.com>
2021-06-21 14:37:42 +0200

    add axon and db config

--- bankacc.query.api/src/main/java/com/springbank/bankacc/query/api/BankAccQueryApplication.java
+++ bankacc.query.api/src/main/java/com/springbank/bankacc/query/api/BankAccQueryApplication.java
@@ -1,9 +1,16 @@
 package com.springbank.bankacc.query.api;
 
+import com.springbank.bankacc.core.config.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Import({AxonConfig.class})
 public class BankAccQueryApplication {
 
 	public static void main(String[] args) {
