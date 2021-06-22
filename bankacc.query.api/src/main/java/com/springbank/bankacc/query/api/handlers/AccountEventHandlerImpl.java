cb04637fe1f93bdcd31c4c9701b20e61facbf89a
ragger.break <ragger.break@gmail.com>
2021-06-22 20:24:17 +0200

    fix NPE AccountRepository Injection

--- bankacc.query.api/src/main/java/com/springbank/bankacc/query/api/handlers/AccountEventHandlerImpl.java
+++ bankacc.query.api/src/main/java/com/springbank/bankacc/query/api/handlers/AccountEventHandlerImpl.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AccountEventHandlerImpl implements AccountEventHandler {
 
-    private AccountRepository accountRepository;
+    private final AccountRepository accountRepository;
 
     @EventHandler
     @Override
